59a2e274a0c630c1a7bb3529ff4b75ece251498e
toine08 <riedotoine@gmail.com>
2024-11-15 05:48:55 +0100

    Add player functionality and circular shape base class

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    fps = pygame.time.Clock()
+    dt= 0
     screen =pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0, 0, 0)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player)
+    drawable.add(player)
+
     while True:
+        screen.fill(color)
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
+        pygame.display.flip()   
         for event in pygame.event.get():
-            screen.fill(color)
-            pygame.display.flip()
             if event.type == pygame.QUIT:
                 return
+        # Limits framerate to 60 FPS
+        dt = fps.tick(60) / 1000
 
         
     print("Starting asteroids!")
